9d318780ea613d92ca7c46299eb28b02f3c239da
Cristiana <cristiana_aso@hotmail.com>
2024-03-05 21:21:10 +0000

    changing docker file for testing

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-    FROM openjdk:17-alpine
+FROM eclipse-temurin:17-jdk-alpine
 VOLUME /tmp
 COPY target/*.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar","--spring.profiles.active=dev"]
--- src/main/java/com/studio/controller/CourseController.java
+++ src/main/java/com/studio/controller/CourseController.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/course")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "${app.frontend-origin}")
 public class CourseController {
 
     @Autowired
